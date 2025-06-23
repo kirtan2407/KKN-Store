0894cc05815c684786f5d2b240fbb2c4d578fecd
Kirtan <kirtankankotiya2407@gmail.com>
2025-06-23 16:54:02 +0530

    firebase off

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,16 @@
 // ignore_for_file: unused_import
 
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:kkn_store/app.dart';
 import 'package:kkn_store/utils/constants/colors.dart';
 import 'package:kkn_store/utils/theme/theme.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 /// -------------- Entry point of Flutter application -------------
-Future<void> main() async {
+void main() {
   // Todo : Add Widgets Binding
   // Todo : Init Local Storage
   // Todo : Await Native Splash
   // Todo : Initialization Firebase
-
-  // ...
-
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then((FirebaseApp value) => Get.put(AuthenticationRepository()));
   // Todo : Init Firebase Analytics
   // Todo : Initialize Authantication
 
